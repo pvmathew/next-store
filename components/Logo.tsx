988bd74df4f3aa02bac8e162802e4c5b5c29f002
Pavin Mathew <pvmathew@ucdavis.edu>
2021-02-28 23:03:54 +0900

    Added separation line for banner area

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,10 +2,19 @@ import styled from "styled-components";
 import Link from "next/link";
 
 const LogoComponent: React.FC = () => (
-  <Link href="/">
-    <Logo src="https://d1vv73x37cbx43.cloudfront.net/skin/frontend/enterprise/newrhdtheme/css/logocss/CurrentSVGLogo/rhd_logo.svg" />
-  </Link>
+  <Container>
+    <Link href="/">
+      <Logo src="https://d1vv73x37cbx43.cloudfront.net/skin/frontend/enterprise/newrhdtheme/css/logocss/CurrentSVGLogo/rhd_logo.svg" />
+    </Link>
+  </Container>
 );
+
+const Container = styled.div`
+/* background-color: black; */
+margin: 0 50px;
+border-bottom: solid 1px #eee;
+margin-bottom: 20px;
+`
 
 const Logo = styled.img`
   width: 300px;
