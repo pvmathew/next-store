da3ee195a5fc65648251d4fec4ffa15ff157ad5b
Pavin Mathew <pvmathew@ucdavis.edu>
2021-03-02 00:51:46 +0900

    Fix logo de-centering on mobile screen resolution

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -13,16 +13,24 @@ const Header: React.FC = () => (
 );
 
 const Container = styled.div`
-  margin: 20px 50px;
   padding: 60px auto 60px auto;
   border-bottom: solid 1px #eee;
+  margin: 20px 20px;
+
+  @media only screen and (min-width: 768px) {
+    margin: 20px 50px;
+  }
 `;
 
 const Logo = styled.img`
-  width: 300px;
   display: block;
   margin: 80px auto 60px auto;
   cursor: pointer;
+  width: 240px;
+
+  @media only screen and (min-width: 768px) {
+    width: 300px;
+  }
 `;
 
 export default Header;
